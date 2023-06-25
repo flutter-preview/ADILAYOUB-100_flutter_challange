2bdb72fdcc4d1ccbbbfd6841d9154ce528de46bc
ADILAYOUB <mintofree840@gmail.com>
2023-06-25 23:47:57 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,19 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutte_challange/services/auth.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
-import 'firebase_options.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MyApp());
+void main() {
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Friends Chat',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          primaryColor: const Color(0xFFFe5a76),
-          scaffoldBackgroundColor: Colors.white),
-      home: UserAuth().handleAuth(),
-    );
+    return const Placeholder();
   }
 }
